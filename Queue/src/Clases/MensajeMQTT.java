4c11385355eb48c2e588e983e08c50faea786ddf
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-09-26 11:49:06 -0500

    update MensajeMQTT.java

--- Queue/src/Clases/MensajeMQTT.java
+++ Queue/src/Clases/MensajeMQTT.java
@@ -1,41 +1,40 @@
-/*
- */
 package Clases;
-
 /**
- *
- * @author USER
+ Primera Juan 6902510051
+ Herrera Alejandro 6902510014
+ Aaron Hernandez 6902510046
  */
 public class MensajeMQTT {
     private final int id;
-    private final Sensor dispositivo;
+    private final String dispositivoId;
+    private final String topic;
     private final String payload;
-    private final String timestamp; 
-    // atributos de la clase MensajeMQTT
+    private final String timestamp;
+    
+   // atributos de la clase MensajeMQTT
 
-    public MensajeMQTT(int id, Sensor dispositivo, String payload, String timestamp) {
+       public MensajeMQTT(int id, Sensor sensor, String payload, String timestamp) {
         this.id = id;
-        this.dispositivo = dispositivo;
+        this.dispositivoId = sensor.name(); // pasa S01,S02 y S03
+        this.topic = sensor.getTopic();
         this.payload = payload;
         this.timestamp = timestamp;
     }
-
     public int getId() { return id; }
-    public Sensor getDispositivo() { return dispositivo; }
+    public String getDispositivoId() { return dispositivoId; }
+    public String getTopic() { return topic; }
     public String getPayload() { return payload; }
     public String getTimestamp() { return timestamp; }
     // getters de la clase MensajeMQTT 
 
-    @Override
-    public String toString() {
-        return "MensajeMQTT:" + "\n" + 
-        "ID:" + id + "\n" + 
-        "Dispositivo:" + dispositivo + "\n" + 
-        "Payload:" + payload + "\n" + 
-        "Timestamp:" + timestamp + "\n" + 
-        "Topic:" + dispositivo.getTopic();
+     @Override
+        public String toString() {
+        return "\n--- MensajeMQTT ---" +
+               "\nID: " + id +
+               "\nDispositivo: " + dispositivoId +
+               "\nTopic: " + topic +
+               "\nPayload: " + payload +
+               "\nTimestamp: " + timestamp;
     }
     // mensaje de salida de la clase MensajeMQTT
-    
-    
 }
